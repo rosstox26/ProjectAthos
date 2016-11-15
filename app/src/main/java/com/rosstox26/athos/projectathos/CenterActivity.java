2130bf993b80447353a6595ab9f5ca3ffbc9c843
daviburu <daviburu@umich.edu>
2016-11-15 14:07:56 -0500

    Create New Screens

---
+++ app/src/main/java/com/rosstox26/athos/projectathos/CenterActivity.java
@@ -0,0 +1,13 @@
+package com.rosstox26.athos.projectathos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_center);
+    }
+}
---
+++ app/src/main/java/com/rosstox26/athos/projectathos/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.rosstox26.athos.projectathos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
