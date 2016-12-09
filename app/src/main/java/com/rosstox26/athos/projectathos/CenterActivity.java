1e3cf2029d502e544ecfae8dc222a870d52decd3
sarielle <sarielle@umich.edu>
2016-12-09 14:11:46 -0500

    Code for Welcome Activity

--- app/src/main/java/com/rosstox26/athos/projectathos/CenterActivity.java
+++ app/src/main/java/com/rosstox26/athos/projectathos/CenterActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class CenterActivity extends Activity implements View.OnClickListener {
-
+//
     private Button buttonWriteReview;
     private Button buttonMore1;
     private Button buttonMore2;
